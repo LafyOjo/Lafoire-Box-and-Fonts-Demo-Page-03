a1e89013a4d132bbe9f44ba5261362fb98f42fca
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2022-06-14 00:22:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+docuemnt.querySelector('.toggle').addEventListener('click', () => {
+    docuemnt.querySelector('.toggle').classList.toggle('active');
+    document.querySelector('.showcase').classList.toggle('active');
+})
